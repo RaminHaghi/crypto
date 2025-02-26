82d1918f95d99ee3aa733473b5c19bcfc3ec5d2f
Ramin <raminhaghi98@gmail.com>
2025-02-26 17:51:42 +0330

    start coding for sign-in and sign-up window

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,12 +5,14 @@ const menuCrossClose = document.querySelector('.nav-close-btn');
 
 function mobileNavOpen() {
     hamburgerMenu.classList.add('menu-open');
+    hamburgerMenu.classList.add('hidden');
     backgroundBlur.classList.remove('hidden');
     navMobile.style.right = '0';
 }
 
 function closeNavMobile() {
     hamburgerMenu.classList.remove('menu-open');
+    hamburgerMenu.classList.remove('hidden');
     backgroundBlur.classList.add('hidden');
     navMobile.style.right = '-22.5rem';
 }
